7078d00f794cd18755ccb2442e94efe2eb5d00fd
Isaias Cordova <igerardo0808@gmail.com>
2023-08-25 02:40:48 -0600

    Archivos iniciales

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// Importando las librerias necesarias
+import { BrowserRouter } from "react-router-dom";
+import {Contact, Experience,About, Feedbacks, Hero, Tech, Works, StarsCanvas, Navbar} from './components';
+import './index.css'
+
+function App() {
+  return (
+        <BrowserRouter>
+          <div className="relative z-0 bg-primary">
+            <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+              <Navbar />
+              <Hero />
+            </div>
+            <About />
+            <Experience />
+            <Tech />
+            <Works />
+            <Feedbacks />
+            <div className="relative z-0">
+              <Contact />
+              <StarsCanvas />
+              
+            </div>
+          </div>
+        </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import {Canvas} from '@react-three/fiber';
+import { OrbitControls,Preload,useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
+
+
+const Computers = ({isMobile}) => {
+  const computer = useGLTF('../public/desktop_pc/scene.gltf');
+  
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.50}
+      groundColor="black" />
+      <pointLight intensity={0.88} />
+      <spotLight position={[-20,50,10]} angle={0.12} penumbra={1}
+      intensity={1}
+      castShadow
+      shadow-mapSize={1024}/>
+      <primitive object={computer.scene} 
+      scale={isMobile ? 0.5 : 0.75}
+      position={isMobile ? [0,-3,-2.5] : [0,-3.25, -1.5]}
+      rotation={[-0.01, -0.2, -0.1]}/>
+    </mesh>
+  )
+}
+
+export default function ComputersCanvas ()  {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width:500)');
+    setIsMobile(mediaQuery.matches);
+
+    // Set the initial value of the 'isMobile' state variable
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    // Define a callback function to handle changes to the media query
+    mediaQuery.addEventListener('change',
+    handleMediaQueryChange);
+
+    // Add the callback function as a listener for changes to the media query
+    return () => {
+      mediaQuery.removeEventListener('change',
+      handleMediaQueryChange);
+    }
+  },[])
+  return(
+    <Canvas
+    frameloop='demand'
+    shadows
+    camera={{position: [20,3,5], fov:25}}
+    gl={{preserveDrawingBuffer: true}}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} 
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}/>
+      
+      <Computers isMobile={isMobile} />
+      </Suspense>
+      
+    </Canvas>
+  )
+}
