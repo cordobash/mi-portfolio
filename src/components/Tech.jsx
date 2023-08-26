1360c32430963d8b02f51d7975decb1c9b041269
Isaias Cordova <igerardo0808@gmail.com>
2023-08-26 17:22:23 -0600

    Work component working

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+// import {BallCanvas} from '../components/canvas/Ball';
+import BallCanvas from '../components/canvas/Ball'
+import { SectionWrapper } from '../hoc'
+import { textVariant } from '../utils/motion'
+import { technologies } from '../constants'
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-wrap flex-row justify-center gap-10'>
+      {technologies.map((tech, index) => (
+        <div className='w-28 h-28' key={tech.name}>
+            <BallCanvas icon={tech.icon} />
+          </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech,'tech');
