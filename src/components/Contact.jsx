11e1dc144d7580e0d70db00dbb636b6639897a02
Isaias Cordova <igerardo0808@gmail.com>
2023-08-26 21:03:48 -0600

    Contact Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+import React,{useState,useEffect,useRef} from 'react'
+
+import { motion } from 'framer-motion'
+import emailjs from '@emailjs/browser';
+import { styles } from '../styles';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name:'',
+    email:'',
+    message:'',
+  })
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSubmit = (e) => {
+    
+  }
+
+  
   return (
-    <div>Contact</div>
+    <div
+      className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'
+    >
+      <motion.div
+        variants={slideIn('left','tween',0.2,1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.heroSubText}>Get in touch</p>
+        <h3 className={styles.heroHeadText}>Contact</h3>
+
+
+      <form
+        ref={formRef}
+        onSubmit={handleSubmit}
+        className="mt-12 flex flex-col gap-8"
+      >
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your name</span>
+            {/* Name */}
+            <input 
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary
+              text-white rounded-lg outlined-none border-none font-medium'
+            />
+          </label>
+          {/* Email */}
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your email</span>
+            <input 
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="miemail@example.com"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary
+              text-white rounded-lg outlined-none border-none font-medium'
+            />
+          </label>
+          {/*  */}
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your email</span>
+            <textarea
+              rows="7"
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary
+              text-white rounded-lg outlined-none border-none font-medium'
+            />
+          </label>
+
+          <button
+            type='submit'
+            onClick={(e) => {
+              e.preventDefault()
+              alert('La informacion de contacto ha sido enviada')   
+            }}
+
+            className='bg-tertiary py-3 px-8 outline-none text-white font-bold
+            shadow-md shadow-primary rounded-xl'
+
+          >
+            {loading ? 'Sending...' : 'Send'}
+
+          </button>
+      </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn('left',"tween",0.2,1)} 
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+
+      </motion.div>
+
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact,'contact')
